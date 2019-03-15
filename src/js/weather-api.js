34fefb9fd860342f66c101996874c51ced85f607
Agata <kozinska.agata@gmail.com>
2019-03-15 01:42:38 +0100

    Handle errors

--- src/js/weather-api.js
+++ src/js/weather-api.js
@@ -8,12 +8,14 @@ export default class Weather {
 		const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${this.apiKey}&units=metric`;
 		try {
 			const resp = await fetch(url);
-			const data = await resp.json();
-			//display message when a city is not found
-			if (data.cod == 404) {
-				showMessage('Something went wrong...' + data.message);
-			} else {
+			if (resp.ok) {
+				const data = await resp.json();
 				return data;
+			}
+			if (!resp.ok && resp.status === 404) {
+				showMessage('Something went wrong...' + (await resp.json()).message);
+			} else {
+				throw new Error(`Request rejected with status ${resp.status}`);
 			}
 		} catch (err) {
 			throw new Error(`fetch failed: ${err}`);
